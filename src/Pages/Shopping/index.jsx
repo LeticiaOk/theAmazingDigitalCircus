c3da873d16ea1cee67a118c7fd84122bc47478a7
LETICIA ALVES DE SOUZA DIAS <leticia.asdias1@senacsp.edu.br>
2024-10-08 21:03:41 -0300

    organizando usestate e tamanho das imagens

--- src/Pages/Shopping/index.jsx
+++ src/Pages/Shopping/index.jsx
@@ -1,29 +1,33 @@
+import { useState } from "react";
 import data from "../../data/data.json";
 import "./style.css";
 export default function Shopping() {
-  const [currentProduct, setCurrentProduct] = useState();
+
+  const [hoveredProductId, setHoveredProductId] = useState(null);
+
+  const handleMouseEnter = (id) => setHoveredProductId(id);
+  const handleMouseLeave = () => setHoveredProductId(null);
 
   return (
     <>
-        <section className="banner">
-            <img className="collections-banner" src="https://cdn.shopify.com/s/files/1/0786/4903/3026/collections/i.png?v=1702349007" alt="the amazing digital circus" />
-            <h1 className="collections-title">THE AMAZING DIGITAL CIRCUS</h1>
-            {data.map((product, index) =>(
-              <div key={index}>
-                {setCurrentProduct(product)}
-                
-                  {/* <img
-        src={currentImage}
-        alt=""
-        className="product-image"
-        onMouseEnter={() => setCurrentImage(currentProduct.image.preview_after)}
-        onMouseLeave={() => setCurrentImage(currentProduct.image.preview_before)}
-      />
-      <p className="product-name">{currentProduct.name}</p>
-      <p className="product-price">R${currentProduct.price.toFixed(2)}</p> */}
-              </div>
-            ))}
-        </section>
+      <section className="banner">
+        <img className="collections-banner" src="https://cdn.shopify.com/s/files/1/0786/4903/3026/collections/i.png?v=1702349007" alt="the amazing digital circus"></img>
+        <h1 className="collections-title">THE AMAZING DIGITAL CIRCUS</h1>
+      </section>
+
+      <section className="products-collection">
+        {data.map((product, index) => (
+          <div key={index} className="product-collection">
+            <img
+              src={hoveredProductId === product.id ? product.image.collections_after : product.image.collections_before}
+              onMouseEnter={() => handleMouseEnter(product.id)}
+              onMouseLeave={handleMouseLeave}
+              className="product-img-collection" />
+            <p className="product-name">{product.name}</p>
+            <p className="product-price">R${product.price.toFixed(2)}</p>
+          </div>
+        ))}
+      </section>
     </>
   )
 }
